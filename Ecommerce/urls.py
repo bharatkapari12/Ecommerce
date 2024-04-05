d4e45165bdfb04576ed974ccd6875d1066f704f0
bharatkapari12 <bharatkaparikumar@gmail.com>
2024-04-05 23:51:25 +0545

    now

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from ecomm_app.views import index, contact
@@ -22,4 +24,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- items/admin.py
+++ items/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from items.models import Category
+from items.models import Category, Item
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Item)
